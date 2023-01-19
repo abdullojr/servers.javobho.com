c8d4b0dca013851b027df415825a6badfecde8de
abdullojr <iamnozimjon@gmail.com>
2023-01-19 13:48:58 +0500

    +

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/posts", (req, res) => {
+  res.send("FORMATION");
+});
+
 app.listen(3000, () => {
   console.log("SERVER IS OPEN");
 });
